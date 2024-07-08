ad4625d4bcc1ec3e174d3b4b610070b1d4f0ea1b
BlitzkreckBoys <theasamad6@gmail.com>
2024-07-08 12:38:17 -0400

    added enter button delete button

--- keyboard.js
+++ keyboard.js
@@ -3,3 +3,14 @@ function typeKey(character) {
     output.innerText += character;
 
 }
+function backspace() {
+    const output = document.getElementById('output');
+    output.innerText = output.innerText.slice(0, -1);
+}
+
+function space() {
+    const output = document.getElementById('output');
+    output.innerText  += "  ";
+}
+
+
